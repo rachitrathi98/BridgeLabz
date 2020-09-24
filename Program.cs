73397d97ef9a2780bdc8b7f5e1b3d4839446fc1d
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-24 12:41:09 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num = 5;
+            int num1 = 6;
+            Console.WriteLine("HelloWorld");
+            
+        }
+    }
+}
